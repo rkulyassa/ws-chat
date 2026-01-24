8d42968f8bf79c9cf097e3c95f1db174d88795de
Ryan Kulyassa <rkulyassa@gmail.com>
2026-01-24 16:36:26 -0500

    working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <App.h>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+    uWS::App()
+    // .get("/*", [](auto *res, auto */*req*/) {
+    //     res->end("Hello world!");
+    // })
+    .ws<nullptr_t>("/*", {
+        .open = [](auto *ws) {
+            std::cout << "Client connected\n";
+        },
+        .message = [](auto *ws, std::string_view msg, uWS::OpCode opCode) {
+            ws->send(msg, opCode); // echo back
+        },
+        .close = [](auto *ws, int /*code*/, std::string_view /*msg*/) {
+            std::cout << "Client closed\n";
+        }
+    })    
+    .listen(3000, [](auto *listen_socket) {
+        if (listen_socket) {
+            std::cout << "Listening on port " << 3000 << std::endl;
+        }
+    }).run();
+
+    std::cout << "Failed to listen on port 3000" << std::endl;
 }
