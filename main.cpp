6287f2835befa54622d2449bc508554eb90ba8b7
Ryan Kulyassa <rkulyassa@gmail.com>
2026-01-25 09:49:43 -0500

    serve client

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <App.h>
+#include <fstream>
 
 struct ClientData {
     int clientId;
 };
+
+std::string readFile(const std::string &path) {
+    std::ifstream file(path, std::ios::binary);
+    std::ostringstream ss;
+    ss << file.rdbuf();
+    return ss.str();
+}
 
 int main() {
     std::vector<uWS::WebSocket<false, true, ClientData>*> clients;
@@ -11,15 +19,20 @@ int main() {
     int clientIndex = 1;
 
     uWS::App()
-    // .get("/*", [](auto *res, auto */*req*/) {
-    //     res->end("Hello world!");
-    // })
+    .get("/*", [](auto *res, auto */*req*/) {
+        res->writeHeader("Content-Type", "text/html; charset=utf-8");
+        res->end(readFile("public/index.html"));
+    })
+    .get("/main.js", [](auto *res, auto */*req*/) {
+        res->writeHeader("Content-Type", "application/javascript");
+        res->end(readFile("public/main.js"));
+    })
     .ws<ClientData>("/*", {
         .open = [&clients, &clientIndex](auto *ws) {
             std::cout << "Client connected\n";
             ws->getUserData()->clientId = clientIndex++;
             clients.push_back(ws);
-            std::string joinMessage = "[SERVER]: Client " + std::to_string(ws->getUserData()->clientId) + " joined\n";
+            std::string joinMessage = "[SERVER]: Client " + std::to_string(ws->getUserData()->clientId) + " joined";
             for (auto *c : clients) {
                 c->send(joinMessage, uWS::OpCode::TEXT);
             }
@@ -32,8 +45,13 @@ int main() {
                 c->send(broadcastMessage, uWS::OpCode::TEXT);
             }
         },
-        .close = [](auto *ws, int /*code*/, std::string_view /*msg*/) {
-            std::cout << "Client closed\n";
+        .close = [&clients](auto *ws, int /*code*/, std::string_view /*msg*/) {
+            std::cout << "Client left\n";
+            int clientId = ws->getUserData()->clientId;
+            std::string broadcastMessage = "[SERVER]: Client " + std::to_string(clientId) + " left";
+            for (auto *c : clients) {
+                c->send(broadcastMessage, uWS::OpCode::TEXT);
+            }
         }
     })    
     .listen(3000, [](auto *listen_socket) {
